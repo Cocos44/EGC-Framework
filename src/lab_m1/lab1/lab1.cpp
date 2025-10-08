6d5cd15e3ae117a65c65b9ea973184785a4ffbc0
Vlad Andrei Grigoras <vladandreigrigoras@gmail.com>
2025-10-08 13:17:31 +0300

    m1::Lab1 solved.

--- src/lab_m1/lab1/lab1.cpp
+++ src/lab_m1/lab1/lab1.cpp
@@ -1,61 +1,86 @@
 #include "lab_m1/lab1/lab1.h"
 
-#include <vector>
-#include <iostream>
+#include <cmath>
+
+#include "glm/fwd.hpp"
 
 using namespace std;
 using namespace m1;
-
 
 /*
  *  To find out more about `FrameStart`, `Update`, `FrameEnd`
  *  and the order in which they are called, see `world.cpp`.
  */
 
+Lab1::Lab1() {
+    // Initiate with a black background.
+    this->backgroundColors = glm::vec3(0, 0, 0);
 
-Lab1::Lab1()
-{
-    // TODO(student): Never forget to initialize class variables!
+    // Start background cycle with 1.
+    this->backgroundCycle = 1;
 
+    // Start mesh cycle with 0.
+    this->meshCycle = 0;
+
+    // Initial position of our mesh.
+    this->meshPosition = glm::vec3(-3, 1, 1);
+    this->rotationAngle = 0;
 }
 
+Lab1::~Lab1() {}
 
-Lab1::~Lab1()
-{
-}
-
-
-void Lab1::Init()
-{
+void Lab1::Init() {
     // Load a mesh from file into GPU memory. We only need to do it once,
     // no matter how many times we want to draw this mesh.
     {
         Mesh* mesh = new Mesh("box");
-        mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS, "primitives"), "box.obj");
+        mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS,
+                                 "primitives"),
+                       "box.obj");
         meshes[mesh->GetMeshID()] = mesh;
     }
 
-    // TODO(student): Load some more meshes. The value of RESOURCE_PATH::MODELS
-    // is actually a path on disk, go there and you will find more meshes.
+    {
+        Mesh* mesh = new Mesh("teapot");
+        mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS,
+                                 "primitives"),
+                       "teapot.obj");
+        meshes[mesh->GetMeshID()] = mesh;
+    }
 
+    {
+        Mesh* mesh = new Mesh("sphere");
+        mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS,
+                                 "primitives"),
+                       "sphere.obj");
+        meshes[mesh->GetMeshID()] = mesh;
+    }
 }
 
+void Lab1::FrameStart() {}
 
-void Lab1::FrameStart()
-{
+void Lab1::ChooseMeshToRender() {
+    switch (this->meshCycle) {
+        case 1:
+            RenderMesh(meshes["box"], this->meshPosition);
+            break;
+        case 2:
+            RenderMesh(meshes["teapot"], this->meshPosition);
+            break;
+        case 3:
+            RenderMesh(meshes["sphere"], this->meshPosition);
+            break;
+        default:
+            RenderMesh(meshes["teapot"], this->meshPosition);
+    }
 }
 
-
-void Lab1::Update(float deltaTimeSeconds)
-{
+void Lab1::Update(float deltaTimeSeconds) {
     glm::ivec2 resolution = window->props.resolution;
 
     // Sets the clear color for the color buffer
-
-    // TODO(student): Generalize the arguments of `glClearColor`.
-    // You can, for example, declare three variables in the class header,
-    // that will store the color components (red, green, blue).
-    glClearColor(0, 0, 0, 1);
+    glClearColor(this->backgroundColors.x, this->backgroundColors.y,
+                 this->backgroundColors.z, 1);
 
     // Clears the color buffer (using the previously set color) and depth buffer
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -69,83 +94,112 @@ void Lab1::Update(float deltaTimeSeconds)
     // Render the object again but with different properties
     RenderMesh(meshes["box"], glm::vec3(-1, 0.5f, 0));
 
-    // TODO(student): We need to render (a.k.a. draw) the mesh that
-    // was previously loaded. We do this using `RenderMesh`. Check the
-    // signature of this function to see the meaning of its parameters.
     // You can draw the same mesh any number of times.
 
+    /**
+     NOTE: Code for rotating controlled mesh.
+     Calculate rotation matrix and update mesh position.
+
+     this->rotationAngle += deltaTimeSeconds * 0.01f;
+     this->meshPosition =
+        glm::mat3(cos(this->rotationAngle), -1 * sin(this->rotationAngle), 0,
+                  sin(this->rotationAngle), cos(this->rotationAngle), 0, 0,
+                  0, 1) *
+     this->meshPosition;
+     */
+
+    this->ChooseMeshToRender();
 }
 
-
-void Lab1::FrameEnd()
-{
-    DrawCoordinateSystem();
-}
-
+void Lab1::FrameEnd() { DrawCoordinateSystem(); }
 
 /*
  *  These are callback functions. To find more about callbacks and
  *  how they behave, see `input_controller.h`.
  */
 
-
-void Lab1::OnInputUpdate(float deltaTime, int mods)
-{
+void Lab1::OnInputUpdate(float deltaTime, int mods) {
     // Treat continuous update based on input
-
-    // TODO(student): Add some key hold events that will let you move
-    // a mesh instance on all three axes. You will also need to
-    // generalize the position used by `RenderMesh`.
-
-}
-
-
-void Lab1::OnKeyPress(int key, int mods)
-{
-    // Add key press event
-    if (key == GLFW_KEY_F) {
-        // TODO(student): Change the values of the color components.
-
+    if (window->KeyHold(GLFW_KEY_W)) {
+        this->meshPosition.z -= 1.0f * deltaTime;
     }
 
-    // TODO(student): Add a key press event that will let you cycle
-    // through at least two meshes, rendered at the same position.
-    // You will also need to generalize the mesh name used by `RenderMesh`.
+    if (window->KeyHold(GLFW_KEY_A)) {
+        this->meshPosition.x -= 1.0f * deltaTime;
+    }
 
+    if (window->KeyHold(GLFW_KEY_S)) {
+        this->meshPosition.z += 1.0f * deltaTime;
+    }
+
+    if (window->KeyHold(GLFW_KEY_D)) {
+        this->meshPosition.x += 1.0f * deltaTime;
+    }
+
+    if (window->KeyHold(GLFW_KEY_E)) {
+        this->meshPosition.y += 1.0f * deltaTime;
+    }
+
+    if (window->KeyHold(GLFW_KEY_Q)) {
+        this->meshPosition.y -= 1.0f * deltaTime;
+    }
 }
 
+glm::vec3 Lab1::GetBackgroundColor() {
+    switch (this->backgroundCycle) {
+        case 0:
+            return m1::BackgroundType::BLACK;
+        case 1:
+            return m1::BackgroundType::RED;
+        case 2:
+            return m1::BackgroundType::GREEN;
+        case 3:
+            return m1::BackgroundType::BLUE;
+        default:
+            return m1::BackgroundType::BLACK;
+    }
+}
 
-void Lab1::OnKeyRelease(int key, int mods)
-{
+void Lab1::OnKeyPress(int key, int mods) {
+    // Add key press event
+    if (key == GLFW_KEY_F) {
+        this->backgroundColors = this->GetBackgroundColor();
+
+        if (this->backgroundCycle == 3)
+            this->backgroundCycle = 0;
+        else
+            this->backgroundCycle++;
+    }
+
+    if (key == GLFW_KEY_T) {
+        if (this->meshCycle == 3) {
+            this->meshCycle = 1;
+        } else {
+            this->meshCycle++;
+        }
+    }
+}
+
+void Lab1::OnKeyRelease(int key, int mods) {
     // Add key release event
 }
 
-
-void Lab1::OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY)
-{
+void Lab1::OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY) {
     // Add mouse move event
 }
 
-
-void Lab1::OnMouseBtnPress(int mouseX, int mouseY, int button, int mods)
-{
+void Lab1::OnMouseBtnPress(int mouseX, int mouseY, int button, int mods) {
     // Add mouse button press event
 }
 
-
-void Lab1::OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods)
-{
+void Lab1::OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods) {
     // Add mouse button release event
 }
 
-
-void Lab1::OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY)
-{
+void Lab1::OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY) {
     // Treat mouse scroll event
 }
 
-
-void Lab1::OnWindowResize(int width, int height)
-{
+void Lab1::OnWindowResize(int width, int height) {
     // Treat window resize event
 }
--- src/lab_m1/lab1/lab1.h
+++ src/lab_m1/lab1/lab1.h
@@ -1,33 +1,71 @@
 #pragma once
 
 #include "components/simple_scene.h"
+#include "glm/fwd.hpp"
 
+namespace m1 {
+class Lab1 : public gfxc::SimpleScene {
+   public:
+    Lab1();
+    ~Lab1();
 
-namespace m1
-{
-    class Lab1 : public gfxc::SimpleScene
-    {
-     public:
-        Lab1();
-        ~Lab1();
+    void Init() override;
 
-        void Init() override;
+   private:
+    void FrameStart() override;
+    void Update(float deltaTimeSeconds) override;
+    void FrameEnd() override;
 
-     private:
-        void FrameStart() override;
-        void Update(float deltaTimeSeconds) override;
-        void FrameEnd() override;
+    void OnInputUpdate(float deltaTime, int mods) override;
+    void OnKeyPress(int key, int mods) override;
+    void OnKeyRelease(int key, int mods) override;
+    void OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY) override;
+    void OnMouseBtnPress(int mouseX, int mouseY, int button, int mods) override;
+    void OnMouseBtnRelease(int mouseX, int mouseY, int button,
+                           int mods) override;
+    void OnMouseScroll(int mouseX, int mouseY, int offsetX,
+                       int offsetY) override;
+    void OnWindowResize(int width, int height) override;
 
-        void OnInputUpdate(float deltaTime, int mods) override;
-        void OnKeyPress(int key, int mods) override;
-        void OnKeyRelease(int key, int mods) override;
-        void OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY) override;
-        void OnMouseBtnPress(int mouseX, int mouseY, int button, int mods) override;
-        void OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods) override;
-        void OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY) override;
-        void OnWindowResize(int width, int height) override;
+    // Student fields + methods.
+   private:
+    glm::vec3 backgroundColors;
 
-        // TODO(student): Class variables go here
+    // Field used to cycle through meshes ("BOX", "SPHERE", "TEAPOT").
+    int meshCycle;
 
-    };
-}   // namespace m1
+    // Field used to cycle through background colors ("BLACK", "RED", "GREEN",
+    // "BLUE").
+    int backgroundCycle;
+
+    // Position of our controlled mesh.
+    glm::vec3 meshPosition;
+
+    // Used for rotating mesh.
+    float rotationAngle;
+
+   private:
+    /**
+     * @brief Choose a mesh based on the meshCycle field and render it.
+     * @see m1::Lab1->meshCycle
+     */
+    void ChooseMeshToRender();
+
+    /**
+     * @brief Choose a mesh based on the backgroundCycle field.
+     * @return 3Vector containing background color.
+     */
+    glm::vec3 GetBackgroundColor();
+};
+
+/**
+ * @namespace BackgroundType
+ * @brief Contains constant vectors used to choose a background color.
+ */
+namespace BackgroundType {
+static const glm::vec3 BLACK = glm::vec3(0.0f, 0.0f, 0.0f);
+static const glm::vec3 RED = glm::vec3(1.0f, 0.0f, 0.0f);
+static const glm::vec3 GREEN = glm::vec3(0.0f, 1.0f, 0.0f);
+static const glm::vec3 BLUE = glm::vec3(0.0f, 0.0f, 1.0f);
+};  // namespace BackgroundType
+}  // namespace m1
